12cb37979036a356b699443cd52b517c6a6659d0
Elliyas Ahmed <elliyasahmedbd@gmail.com>
2017-03-12 13:27:31 +0600

    Update youtubeframe.js

--- youtubeframe.js
+++ youtubeframe.js
@@ -3,6 +3,6 @@ var iframe_src       = iframe.attr('src');
 var youtube_video_id = iframe_src.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/).pop();
 
 if (youtube_video_id.length == 11) {
-    var video_thumbnail = $('<img class="youtube-img" src="//img.youtube.com/vi/'+youtube_video_id+'/0.jpg"/>');
+    var video_thumbnail = $('<div class="youtube-img"><img src="//img.youtube.com/vi/'+youtube_video_id+'/0.jpg"/></div>');
     $(video_thumbnail).insertBefore("iframe");
 }
